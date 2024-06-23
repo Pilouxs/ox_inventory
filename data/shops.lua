a29cc6ea277eb032fc404c96e66e2fe844a2f32e
Pilouxs <69396689+Pilouxs@users.noreply.github.com>
2024-06-23 19:55:56 +0200

    Sync symbiose shops

--- data/shops.lua
+++ data/shops.lua
@@ -1,12 +1,28 @@
 return {
+	Pawnshop = {
+		name = 'Pawnshop',
+		blip = {
+			id = 59, colour = 69, scale = 0.8
+		}, inventory = {
+			{name = 'phone', price = 10},
+			{name = 'binoculars', price = 10},
+			{name = 'radio', price = 10},
+		}, locations = {
+			vec3(25.7, -1347.3, 29.49),
+		}, targets = {
+			{ loc = vec3(451.64, -1473.08, 28.29), length = 2.0, width = 0.7, heading = 65, minZ = 28.29, maxZ = 30.29, distance = 1.5 },
+		}
+	},
+
 	General = {
-		name = 'Shop',
+		name = '24/7',
 		blip = {
 			id = 59, colour = 69, scale = 0.8
 		}, inventory = {
 			{ name = 'burger', price = 10 },
 			{ name = 'water', price = 10 },
 			{ name = 'cola', price = 10 },
+			{ name = 'phone', price = 10 },
 		}, locations = {
 			vec3(25.7, -1347.3, 29.49),
 			vec3(-3038.71, 585.9, 7.9),
@@ -33,7 +49,7 @@ return {
 	},
 
 	Liquor = {
-		name = 'Liquor Store',
+		name = '24/7 Liqueur',
 		blip = {
 			id = 93, colour = 69, scale = 0.8
 		}, inventory = {
@@ -59,7 +75,7 @@ return {
 	},
 
 	YouTool = {
-		name = 'YouTool',
+		name = 'Bric-à-Brac',
 		blip = {
 			id = 402, colour = 69, scale = 0.8
 		}, inventory = {
@@ -73,7 +89,7 @@ return {
 	},
 
 	Ammunation = {
-		name = 'Ammunation',
+		name = 'Armurerie',
 		blip = {
 			id = 110, colour = 69, scale = 0.8
 		}, inventory = {
@@ -104,66 +120,14 @@ return {
 		}
 	},
 
-	PoliceArmoury = {
-		name = 'Police Armoury',
-		groups = shared.police,
-		blip = {
-			id = 110, colour = 84, scale = 0.8
-		}, inventory = {
-			{ name = 'ammo-9', price = 5, },
-			{ name = 'ammo-rifle', price = 5, },
-			{ name = 'WEAPON_FLASHLIGHT', price = 200 },
-			{ name = 'WEAPON_NIGHTSTICK', price = 100 },
-			{ name = 'WEAPON_PISTOL', price = 500, metadata = { registered = true, serial = 'POL' }, license = 'weapon' },
-			{ name = 'WEAPON_CARBINERIFLE', price = 1000, metadata = { registered = true, serial = 'POL' }, license = 'weapon', grade = 3 },
-			{ name = 'WEAPON_STUNGUN', price = 500, metadata = { registered = true, serial = 'POL'} }
-		}, locations = {
-			vec3(451.51, -979.44, 30.68)
-		}, targets = {
-			{ loc = vec3(453.21, -980.03, 30.68), length = 0.5, width = 3.0, heading = 270.0, minZ = 30.5, maxZ = 32.0, distance = 6 }
-		}
-	},
-
-	Medicine = {
-		name = 'Medicine Cabinet',
-		groups = {
-			['ambulance'] = 0
-		},
-		blip = {
-			id = 403, colour = 69, scale = 0.8
-		}, inventory = {
-			{ name = 'medikit', price = 26 },
-			{ name = 'bandage', price = 5 }
-		}, locations = {
-			vec3(306.3687, -601.5139, 43.28406)
-		}, targets = {
-
-		}
-	},
-
-	BlackMarketArms = {
-		name = 'Black Market (Arms)',
-		inventory = {
-			{ name = 'WEAPON_DAGGER', price = 5000, metadata = { registered = false	}, currency = 'black_money' },
-			{ name = 'WEAPON_CERAMICPISTOL', price = 50000, metadata = { registered = false }, currency = 'black_money' },
-			{ name = 'at_suppressor_light', price = 50000, currency = 'black_money' },
-			{ name = 'ammo-rifle', price = 1000, currency = 'black_money' },
-			{ name = 'ammo-rifle2', price = 1000, currency = 'black_money' }
-		}, locations = {
-			vec3(309.09, -913.75, 56.46)
-		}, targets = {
-
-		}
-	},
-
 	VendingMachineDrinks = {
-		name = 'Vending Machine',
+		name = 'Distributeur automatique',
 		inventory = {
 			{ name = 'water', price = 10 },
 			{ name = 'cola', price = 10 },
 		},
 		model = {
-			`prop_vend_soda_02`, `prop_vend_fridge01`, `prop_vend_water_01`, `prop_vend_soda_01`
+			`prop_vend_soda_02`, `prop_vend_water_01`, `prop_vend_soda_01` --`prop_vend_fridge01`
 		}
 	}
 }
